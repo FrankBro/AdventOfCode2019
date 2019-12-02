7d6222683ac1950e5d289c106fb343c387a1e175
Francois Brodeur <francois.brodeur@adgear.com>
2019-12-01 21:34:29 -0500

    refactor

--- year2015/day1/day1.go
+++ year2015/day1/day1.go
@@ -1,5 +1,12 @@
 package day1
 
+import (
+	"bufio"
+	"os"
+
+	"github.com/FrankBro/AdventOfCodeGo/util"
+)
+
 func findFloor(s string) int {
 	var floor int
 	for _, c := range s {
@@ -33,4 +40,14 @@ func findPosition(s string) int {
 		}
 	}
 	return position
+}
+
+func readInstruction() string {
+	file, err := os.Open("input")
+	util.Check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	line := scanner.Text()
+	return line
 }
--- year2015/day1/day1_test.go
+++ year2015/day1/day1_test.go
@@ -1,8 +1,6 @@
 package day1
 
 import (
-	"bufio"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,23 +16,13 @@ func TestPart1(t *testing.T) {
 	require.EqualValues(t, -1, findFloor("))("))
 	require.EqualValues(t, -3, findFloor(")))"))
 	require.EqualValues(t, -3, findFloor(")())())"))
-	file, err := os.Open("input")
-	require.NoError(t, err)
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	line := scanner.Text()
+	line := readInstruction()
 	require.EqualValues(t, 232, findFloor(line))
 }
 
 func TestPart2(t *testing.T) {
 	require.EqualValues(t, 1, findPosition(")"))
 	require.EqualValues(t, 5, findPosition("()())"))
-	file, err := os.Open("input")
-	require.NoError(t, err)
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	line := scanner.Text()
+	line := readInstruction()
 	require.EqualValues(t, 1783, findPosition(line))
 }
